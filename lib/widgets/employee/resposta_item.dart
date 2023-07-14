603225c70acdbeafe20bc73e1e6b35129ef45934
Rúben Oliveira <ruben06_94@hotmail.com>
2023-07-14 18:06:54 +0100

    on work:

--- lib/widgets/employee/resposta_item.dart
+++ lib/widgets/employee/resposta_item.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
 
-class RespostaItem extends StatelessWidget {
-  const RespostaItem({super.key, required this.resposta, required this.onTap});
+class RespostaItem extends StatefulWidget {
+  const RespostaItem(
+      {super.key,
+      required this.resposta,
+      required this.currentQuestionIndex,
+      required this.onTap});
+
   final String resposta;
-  final void Function() onTap;
+  final int Function(String answer) onTap;
+  final int currentQuestionIndex;
+
+  @override
+  State<RespostaItem> createState() => _RespostaItemState();
+}
+
+class _RespostaItemState extends State<RespostaItem> {
+  bool _selected = false;
+
+  void _checkAnswer(int classificacao) {
+    final String classificacaoString = classificacao.toString();
+    if (widget.resposta.contains(classificacaoString)) {
+      setState(() {
+        _selected = true;
+      });
+    }
+    // TODO: Adicionar a função de verificação de acerto
+  }
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onTap,
+      onPressed: () {
+        final result = widget.onTap(widget.resposta);
+        _checkAnswer(result);
+      },
       style: ElevatedButton.styleFrom(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
-        foregroundColor: Colors.white,
+        foregroundColor: _selected
+            ? Theme.of(context).colorScheme.onTertiary
+            : Theme.of(context).colorScheme.onBackground,
+        backgroundColor: _selected
+            ? Theme.of(context).colorScheme.tertiary
+            : Theme.of(context).colorScheme.background,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(40),
         ),
       ),
       child: Text(
-        resposta,
+        widget.resposta,
         textAlign: TextAlign.center,
       ),
     );
