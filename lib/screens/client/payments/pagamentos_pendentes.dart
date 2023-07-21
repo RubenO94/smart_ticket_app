b24530f22cfc55655d7c53b5d3b28e7a399e39ac
Rúben Oliveira <ruben06_94@hotmail.com>
2023-07-21 18:07:20 +0100

    major update: payments on work

--- lib/screens/client/payments/pagamentos_pendentes.dart
+++ lib/screens/client/payments/pagamentos_pendentes.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smart_ticket/data/dummy_data.dart';
+import 'package:smart_ticket/screens/client/payments/pagamento_web_view.dart';
+import 'package:smart_ticket/widgets/client/pagamento_item.dart';
 
 class PagamentosPendentesScreen extends StatelessWidget {
   const PagamentosPendentesScreen({super.key});
@@ -9,8 +12,34 @@ class PagamentosPendentesScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pagamentos Pendentes'),
       ),
-      body: const Center(
-        child: Text('Pagamentos Pendentes'),
+      body: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 48, bottom: 16, right: 16, left: 16),
+            child: Text(
+              'Selecione os pagamentos que deseja realizar',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: pagamentos.length,
+              itemBuilder: (context, index) =>
+                  PagamentoItem(pagamento: pagamentos[index]),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.payments_rounded),
+        label: const Text('Efetuar Pagamento'),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const PagamentoWebViewScreen()));
+        },
       ),
     );
   }
